784f4b5afb9253dbe58ce73f2fb5ae2b6da9ce9f
Ravi Choudhary <151042374+jatravi@users.noreply.github.com>
2026-03-19 14:38:54 +0530

    Set up Express server with basic route
    Fix variable name and error handling in server setup.

---
+++ index-19-03.js
@@ -0,0 +1,12 @@
+const express = require ('express')
+const app = express();
+const PORT = 3000;
+
+app.get('/',(req,res) => {
+  res.send("TEST CASE")
+});
+
+app.listen(POST , (error) => {
+  if (!errir) console.log('Server is running successfully on port', $(POST))
+  else console.log("Server has not started")
+});
